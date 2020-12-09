5c17116351dc9a9564f493453847ddb5aaafaca7
Ashik <ashikdas.cse@gmail.com>
2020-12-09 20:27:47 +0100

    # movie list api integrated with fragment using recycler view adapter. # movie details api integrated with fragment.

--- app/src/main/java/com/mobilecastle/moviedb/views/moviedetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/mobilecastle/moviedb/views/moviedetails/MovieDetailsFragment.kt
@@ -5,9 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.mobilecastle.moviedb.R
+import de.mobilecastle.moviedb.api.TheMovieDBClient
+import de.mobilecastle.moviedb.api.TheMovieDBInterface
+import de.mobilecastle.moviedb.data.MovieDetails
+import de.mobilecastle.moviedb.repository.MovieDetailsRepository
+import de.mobilecastle.moviedb.utilities.AppConstants.POSTER_BASE_URL
+import de.mobilecastle.moviedb.utilities.NetworkState
+import de.mobilecastle.moviedb.viewmodels.MovieDetailsViewModel
+import kotlinx.android.synthetic.main.movie_details_fragment.*
+import java.text.NumberFormat
+import java.util.*
 
 class MovieDetailsFragment : Fragment() {
+
+    private lateinit var viewModel: MovieDetailsViewModel
+    private lateinit var movieRepository: MovieDetailsRepository
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +32,57 @@ class MovieDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.movie_details_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val movieId: Int? = arguments?.getInt("movieId")
+
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+        movieRepository = MovieDetailsRepository(apiService)
+
+        viewModel = movieId?.let { getViewModel(it) }!!
+
+        viewModel.movieDetails.observe(viewLifecycleOwner, Observer {
+            bindUI(it)
+        })
+
+        viewModel.networkState.observe(viewLifecycleOwner, Observer {
+            progress_bar_details.visibility =
+                if (it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error.visibility = if (it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+        })
+    }
+
+    fun bindUI(it: MovieDetails) {
+        movie_title.text = it.title
+        movie_tagline.text = it.tagline
+        movie_release_date.text = it.releaseDate
+        movie_rating.text = it.avgRating.toString()
+        movie_duration.text = it.runtime.toString() + " minutes"
+        movie_overview.text = it.overview
+
+        val formatCurrency = NumberFormat.getCurrencyInstance(Locale.US)
+        movie_budget.text = formatCurrency.format(it.budget)
+        movie_revenue.text = formatCurrency.format(it.revenue)
+
+        val moviePosterURL = POSTER_BASE_URL + it.posterPath
+        Glide.with(this)
+            .load(moviePosterURL)
+            .into(iv_movie_poster);
+
+
+    }
+
+
+    private fun getViewModel(movieId: Int): MovieDetailsViewModel {
+        return ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MovieDetailsViewModel(movieRepository, movieId) as T
+            }
+        })[MovieDetailsViewModel::class.java]
     }
 }
--- app/src/main/java/com/mobilecastle/moviedb/views/movielist/MovieListFragment.kt
+++ app/src/main/java/com/mobilecastle/moviedb/views/movielist/MovieListFragment.kt
@@ -1,13 +1,29 @@
 package com.mobilecastle.moviedb.views.movielist
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.mobilecastle.moviedb.R
+import de.mobilecastle.moviedb.api.TheMovieDBClient
+import de.mobilecastle.moviedb.api.TheMovieDBInterface
+import de.mobilecastle.moviedb.repository.MoviePagedListRepository
+import de.mobilecastle.moviedb.utilities.NetworkState
+import de.mobilecastle.mvvmsampleapp.view.MovieList.MovieListViewModel
+import de.mobilecastle.mvvmsampleapp.view.MovieList.MoviePagedListAdapter
+import kotlinx.android.synthetic.main.movie_list_fragment.*
 
 class MovieListFragment : Fragment() {
+
+    private lateinit var viewModel: MovieListViewModel
+
+    lateinit var movieRepository: MoviePagedListRepository
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +31,57 @@ class MovieListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.movie_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val context = activity as Context
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+
+        movieRepository = MoviePagedListRepository(apiService)
+
+        viewModel = getViewModel()
+
+        val movieAdapter = MoviePagedListAdapter(context)
+
+        val gridLayoutManager = GridLayoutManager(context, 3)
+
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                return if (viewType == movieAdapter.MOVIE_VIEW_TYPE) 1    // Movie_VIEW_TYPE will occupy 1 out of 3 span
+                else 3                                              // NETWORK_VIEW_TYPE will occupy all 3 span
+            }
+        };
+
+
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+
+        viewModel.moviePagedList.observe(viewLifecycleOwner, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(viewLifecycleOwner, Observer {
+            progress_bar_popular.visibility =
+                if (viewModel.isListEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility =
+                if (viewModel.isListEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.isListEmpty()) {
+                movieAdapter.setNetworkState(it)
+            }
+        })
+    }
+
+    private fun getViewModel(): MovieListViewModel {
+        return ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MovieListViewModel(movieRepository) as T
+            }
+        })[MovieListViewModel::class.java]
     }
 }
